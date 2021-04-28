269e41553485e7d9151cbc6652a7231e8cb07543
collinte <collinte@lafayette.edu>
2021-04-28 13:02:21 -0400

    Added MAIA Markov scripts to the repo.

---
+++ algorithms/run_maia_markov/aisc2021_bass_analyse.js
@@ -0,0 +1,197 @@
+// Copyright Tom Collins, 25.4.2021
+// Analysing MIDI files to create state-transition matrix and initial
+// distribution for bassline representations for AI Song Contest 2021 project.
+
+// Requires.
+const fs = require("fs")
+const path = require("path")
+// const uu = require("uuid/v4")
+const { Midi } = require('@tonejs/midi')
+const mu = require("maia-util")
+const mm = require("maia-markov")
+const an = new mm.Analyzer()
+const ch = require("./dexplore/cv_and_hist.js")
+const tx = require("./dexplore/track_extract_util.js")
+
+// Individual user paths.
+const mainPaths = {
+  "tom": {
+    "midi": path.join(
+      "/Users", "tomthecollins", "Shizz", "York", "Projects", "AI\ Eurovision",
+      "2021", "io", "midi"
+    ),
+    "midiDirs": ["midi_1"],
+    "outputDir": path.join(
+      "/Users", "tomthecollins", "Shizz", "York", "Projects", "AI\ Eurovision",
+      "2021", "io", "stm"
+    ),
+    "outputFileName": "aisc2021_bass"
+  },
+  "anotherUser": {
+    // ...
+  }
+}
+
+// Parameters
+const param = {
+  "stateType": "beat_rel_sq_MNN_state",
+  "onAndOff": false,
+  "squashRangeMidi": 12,
+  "nosConsecutives": 4,
+  "downbeat": {
+    "histType": "drumsTrueVelocityTrue",
+    "drumsOnly": true,
+    "rounding": true,
+    "granularity": 4,
+    "beatsInMeasure": 4,
+    "velocityIndex": 4,
+    "ontimeIndex": 0
+  }
+}
+
+// Grab user name from command line to set path to data.
+let nextU = false
+let mainPath;
+process.argv.forEach(function(arg, ind){
+  if (arg === "-u"){
+    nextU = true
+  }
+  else if (nextU){
+    mainPath = mainPaths[arg]
+    nextU = false
+  }
+})
+// fs.mkdir(outdir)
+
+// Import and analyse the MIDI files.
+let midiDirs = fs.readdirSync(mainPath["midi"])
+midiDirs = midiDirs.filter(function(midiDir){
+  return mainPath["midiDirs"].indexOf(midiDir) >= 0
+})
+console.log("midiDirs:", midiDirs)
+midiDirs.forEach(function(midiDir, jDir){
+  console.log("Working on midiDir:", midiDir, "jDir:", jDir)
+  let comps = []
+  let files = fs.readdirSync(path.join(mainPath["midi"], midiDir))
+  files = files.filter(function(file){
+    return file.split(".")[3] == "mid"
+  })
+  console.log("files.length:", files.length)
+
+  files.forEach(function(file, iFile){
+    console.log("file:", file)
+    if (iFile % 10 === 0){
+      console.log("FILE " + (iFile + 1) + " OF " + files.length + ".")
+    }
+    try {
+      const midiData = fs.readFileSync(
+        path.join(mainPath["midi"], midiDir, file)
+      )
+      const midi = new Midi(midiData)
+      let btracks = tx.find_bass_track(midi, false)
+      console.log("btracks:", btracks)
+      if (btracks !== undefined){
+        btracks = btracks.map(function(d){ return d[0] })
+        const timeSigs = [midi.header.timeSignatures.map(function(ts){
+          return {
+            "barNo": ts.measures + 1,
+            "topNo": ts.timeSignature[0],
+            "bottomNo": ts.timeSignature[1],
+            "ontime": ts.ticks/midi.header.ppq
+          }
+        })[0]] // SUPER HACKY. REVISE LATER!
+        if (timeSigs[0].topNo !== 4){
+          console.log("timeSigs:", timeSigs)
+        }
+        let allPoints = [], dbPoints = [], tonalPoints = [], trgPoints = []
+        midi.tracks.forEach(function(track, idx){
+          // console.log("track.instrument.family:", track.instrument.family)
+          // console.log("track.instrument.name:", track.instrument.name)
+          track.notes.forEach(function(n){
+            let pt = [
+              n.ticks/midi.header.ppq,
+              n.midi,
+              n.durationTicks/midi.header.ppq,
+              track.channel,
+              Math.round(1000*n.velocity)/1000
+            ]
+            allPoints.push(pt)
+            if (
+              !param.downbeat.drumsOnly ||
+              param.downbeat.drumsOnly && track.instrument.family === "drums"
+            ){
+              dbPoints.push(pt)
+            }
+            if (track.instrument.family !== "drums"){
+              tonalPoints.push(pt)
+            }
+            if (btracks.indexOf(idx) >= 0){
+              trgPoints.push(pt)
+            }
+          })
+        })
+        if (dbPoints.length === 0) {
+          console.log("No downbeat detection points. Reverting to full dataset!")
+          dbPoints = allPoints
+        }
+        dbPoints = mu.copy_array_object(mu.sort_rows(dbPoints)[0])
+        tonalPoints = mu.copy_array_object(mu.sort_rows(tonalPoints)[0])
+        console.log("trgPoints.length:", trgPoints.length)
+        trgPoints = mu.copy_array_object(mu.sort_rows(trgPoints)[0])
+        // console.log("trgPoints.slice(0, 5):", trgPoints.slice(0, 5))
+
+        // Downbeat estimation and correction
+        const dbCorrect = ch.correct_downbeat(dbPoints, param.downbeat)
+        // console.log("dbCorrect.confidence:", dbCorrect.confidence)
+        // console.log("dbCorrect.startsOnBeatEst:", dbCorrect.startsOnBeatEst)
+        // console.log(dbCorrect.pointSet.slice(0, 3))
+        tonalPoints.forEach(function(p){
+          p[param.downbeat.ontimeIndex] += param.downbeat.beatsInMeasure
+          - dbCorrect.startsOnBeatEst + 1
+        })
+        trgPoints.forEach(function(p){
+          p[param.downbeat.ontimeIndex] += param.downbeat.beatsInMeasure
+          - dbCorrect.startsOnBeatEst + 1
+        })
+
+        // Key detection
+        const fsm = mu.fifth_steps_mode(tonalPoints, mu.krumhansl_and_kessler_key_profiles)
+        // console.log("fsm:", fsm)
+        trgPoints.forEach(function(p){
+          p.splice(2, 0, mu.guess_morphetic(p[1], fsm[2], fsm[3]))
+        })
+        let comp = an.note_point_set2comp_obj(
+          trgPoints, timeSigs, false, [0, 1/4, 1/3, 1/2, 2/3, 3/4, 1]//, [0, 1/6, 1/4, 1/3, 1/2, 2/3, 3/4, 5/6, 1]
+        );
+        // console.log("comp.notes.length:", comp.notes.length)
+        // console.log("comp.notes.slice(0, 3):", comp.notes.slice(0, 3))
+        comp["id"] = "" + iFile
+        comp["name"] = "" + iFile
+        comp["composers"] = [{"id": "default_composer", "name": "none", "displayName": "None"}]
+        comps.push(comp)
+        console.log("")
+      } // if (btracks !== undefined){
+    }
+    catch (e) {
+      console.log(e)
+    }
+  })
+
+  // Construct stm and initial distribution.
+  let stm = an.construct_stm(comps, param)
+  // console.log("pStm.length:", pStm.length);
+  stm = an.prune_stm(stm, param)
+  // console.log("pStm[0].beat_mnn_state:", pStm[0].beat_mnn_state);
+  // console.log("pStm.slice(0, 1):", pStm.slice(0, 1));
+  fs.writeFileSync(
+    path.join(mainPath["outputDir"], mainPath["outputFileName"] + ".js"),
+    JSON.stringify(stm)//, null, 2)
+  )
+
+  let initialDistbn = an.construct_initial(comps, param)
+  initialDistbn = an.prune_initial(initialDistbn, stm, param)
+  fs.writeFileSync(
+    path.join(mainPath["outputDir"], mainPath["outputFileName"] + "_initial.js"),
+    JSON.stringify(initialDistbn)//, null, 2)
+  )
+})
---
+++ algorithms/run_maia_markov/aisc2021_chords_generate.js
@@ -0,0 +1,113 @@
+// Copyright Tom Collins, 27.4.2021
+// Generating chord excerpts for AI Song Contest 2021 project.
+
+// Requires
+const fs = require("fs")
+const path = require("path")
+const sr = require('seed-random')
+const { Midi } = require('@tonejs/midi')
+const mm = require("maia-markov")
+const gn = new mm.Generator()
+
+// Individual user paths
+const mainPaths = {
+  "tom": {
+    "stm": path.join(
+      "/Users", "tomthecollins", "Shizz", "York", "Projects", "AI\ Eurovision",
+      "2021", "io", "stm", "aisc2021_chords.js"
+    ),
+    "initial": path.join(
+      "/Users", "tomthecollins", "Shizz", "York", "Projects", "AI\ Eurovision",
+      "2021", "io", "stm", "aisc2021_chords_initial.js"
+    ),
+    "outputDir": path.join(
+      "/Users", "tomthecollins", "Shizz", "York", "Projects", "AI\ Eurovision",
+      "2021", "io", "out", "aisc2021_chords"
+    )
+  },
+  "anotherUser": {
+    "stm": "",
+    "initial": "",
+    "outputDir": ""
+  }
+}
+
+// Set up parameters.
+let seeds = [
+  "Lepaute", "Mitchel", "Lovelace", "Winlock", "Cannon",
+  "Lee", "Cave-Browne-Cave", "Wilson", "Clem", "Froelich" ,
+  "Clakre", "Paulson", "Aylard", "Rock", "Lever",
+  "Briggs", "Howard", "Pearsall", "Lamarr", "Meltzer",
+  "Holberton", "Antonelli", "Teitelbaum", "Bartik", "Spence",
+  "Goldstine", "Wescoff", "Lichterman", "Worsely", "Hopper"
+]
+let param = {
+  "stateType": "beat_rel_sq_MNN_state",
+  "pointReconstruction": "rel_sq_MNN",
+  "timeSignatures": [ {"barNo": 1, "topNo": 4, "bottomNo": 4, "ontime": 0} ],
+  "stm": null,
+  "initial": null,
+  "nosConsecutives": 4,
+  "ontimeUpperLimit": 16,
+  "squashRangeMidiMorph": [12, 7],
+  "indices": {
+    "ontime": 0, "MNN": 1, "MPN": 2, "duration": 3, "channel": 4, "velocity": 5
+  },
+  "randCount": 0
+}
+
+// Grab user name from command line to set path to data.
+let nextU = false
+let pathsEtc;
+process.argv.forEach(function(arg, ind){
+  if (arg === "-u"){
+    nextU = true
+  }
+  else if (nextU){
+    pathsEtc = mainPaths[arg]
+    nextU = false
+  }
+})
+// fs.mkdir(outdir);
+
+const stmStr = fs.readFileSync(pathsEtc.stm);
+param.stm = JSON.parse(stmStr);
+const initialStr = fs.readFileSync(pathsEtc.initial);
+param.initial = JSON.parse(initialStr);
+// console.log("stm:", stm)
+
+// Seed random number generation.
+// sr('christianeriksen', {global: true}); // Overrides global Math.random.
+// var numA = Math.random();
+// console.log(numA);
+// sr.resetGlobal();// Reset to default Math.random.
+
+seeds.map(function(seed){
+  // Seed random number generation.
+  sr(seed, {global: true})
+  // Reset randCount, then generate states and points.
+  param.randCount = 0
+  console.log("randCount before get_abs_suggestion:", param.randCount)
+  var gendOutput = gn.get_suggestion(param)
+  console.log("randCount after get_abs_suggestion:", gendOutput.randCount)
+  // console.log("gendOutput.points:", gendOutput.points)
+
+  // Convert points to a key according to the initial state.
+  gendOutput.points = gendOutput.points.map(function(p){
+    p[param.indices.MNN] += gendOutput.stateContextPairs[0].context
+    .tonic_pitch_closest[0] //60
+    p[param.indices.MPN] += gendOutput.stateContextPairs[0].context
+    .tonic_pitch_closest[1] //60
+    return p
+  })
+  // Export to MIDI and text files.
+  const me = new mm.MidiExport(
+    gendOutput.points,
+    path.join(pathsEtc["outputDir"], "chords_" + seed + ".mid"),
+    0.5
+  )
+  fs.writeFileSync(
+    path.join(path.join(pathsEtc["outputDir"], "chords_" + seed + ".txt")),
+    JSON.stringify(gendOutput.stateContextPairs, null, 2)
+  )
+})
